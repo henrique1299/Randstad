be31765fdf36e5659176a39ce319c5880a96012f
Henrique Martins Furtado <henriquemfurtado@hotmail.com>
2021-05-26 16:35:17 -0300

    init

---
+++ Randstad/Models/Convert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Randstad.Models
+{
+    public class Convert
+    {
+        [Key]
+        public int convertId { get; set; }
+        public int id1 { get; set; }
+        public int id2 { get; set; }
+        public double converted { get; set; }
+    }
+}
